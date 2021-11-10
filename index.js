560a4c29c7b1f87e6a9ef9a377b8dc624fbcc291
Riko Sakurauchi <i@riko.love>
2021-11-10 18:04:47 -0500

    :sparkles: add comments

--- index.js
+++ index.js
@@ -31,6 +31,12 @@ async function run () {
         )
         await addLabel(token, owner, repo, issue.number, 'approved')
         await leaveComment(token, owner, repo, issue.number,
+          'To make your repository appear in the app and website, here is what you need to do,\n' +
+          "- Make sure you're not leaving the GitHub repo description blank, which indicates the Xposed module display name.\n" +
+          '- Make sure you have at least one release.\n\n' +
+          "If you complied with those requirements but your repo didn't appear in more than 10 minutes, please file an issue to let us know, thanks!"
+        )
+        await leaveComment(token, owner, repo, issue.number,
           'Welcome `' + title + '`!'
         )
         await closeIssue(token, owner, repo, issue.number)
