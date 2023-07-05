476652de182b75270c0e6ccb5c74760eb6336262
Howard Wu <HowardWu20@outlook.com>
2023-07-05 16:15:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ async function run () {
     }
     if (label === 'invalid') {
       await leaveComment(token, owner, repo, issue.number,
-        'It seems like your request has an invalid package name, please consider another package name. (e.g. io.github. ' + owner + '.[appname]' +
+        'It seems like your request has an invalid package name, please consider another package name. (e.g. io.github. ' + owner + '.[appname])' +
         "If that's not true, please contact a human by " +
         'https://modules.lsposed.org/submission?type=appeal'
       )
