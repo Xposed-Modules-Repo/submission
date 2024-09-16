e0de28126ccb22fca88d6db1b10fd5ea57f13e73
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2024-09-16 22:14:15 +0800

    fix: should ignore bot itself

--- index.js
+++ index.js
@@ -58,6 +58,8 @@ async function manualRequest (token, owner, repo, issueNo) {
 
 async function run () {
   try {
+    if (context.payload.sender.id === 78363386) return // ignore bot
+
     const token = core.getInput('github-token')
     const { owner, repo } = getRepo()
     const issue = await getIssue(token)
